f6ff50b03fdff1c0721098d63ded6f919104c4ea
noidsoup <nicholas.eymann@gmail.com>
2020-02-02 20:29:38 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const mysql = require('mysql');
+
+const server = express();
+const port = process.env.PORT || 3000;
+
+var con = mysql.createConnection({
+  host: "localhost",
+  database : 'cactusst_db',
+  user: "root",
+  password: "admin"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+
+  con.query("SELECT * FROM cactus__items where ID=1096", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
+
+server.get("/test", (req, res) => {
+  res.send('it works!');
+});
+
+server.listen(port, () => {
+  console.log("werks");
+});
