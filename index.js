44f7aa31002f618c868a56771c1821d4de1da97e
noidsoup <nicholas.eymann@gmail.com>
2020-02-06 21:31:56 -0800

    creating CSV files from the database

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const mysql = require('mysql');
+const path = require('path');
+const csv = require('csvtojson')
+const fastcsv = require("fast-csv");
+const fs = require("fs");
 
 const server = express();
 const port = process.env.PORT || 3000;
+
+// DOE https://bezkoder.com/node-js-export-mysql-csv-file/ !!!
+// serve static file https://expressjs.com/en/starter/static-files.html
 
 var con = mysql.createConnection({
   host: "localhost",
@@ -12,19 +19,53 @@ var con = mysql.createConnection({
   password: "admin"
 });
 
-/* con.connect(function(err) {
-  if (err) throw err;
+const query = (table) => {
+  return new Promise((resolve, reject) => {
+    con.query(`SELECT * FROM ${table}`, function (err, data, fields) {
+      if (err) throw err;
+  
+      const jsonData = JSON.parse(JSON.stringify(data));
+      
+      fastcsv
+        .write(jsonData, { headers: true })
+        .on("finish", function() {
+          console.log(`Wrote to ${table}.csv`);
+          resolve();
+        })
+        .pipe(fs.createWriteStream(`csv/${table}.csv`));
+  
+        
+    });
+  }) 
+};
 
-  con.query("SELECT * FROM cactus__items where ID=1096", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-  });
-}); */
+const formatData = (items, options) => {
+  console.log("here's where the formatting happens");
+
+/*   const formattedData = [];
+  
+  items.forEach(element => {
+    console.log(element.ID);
+
+
+    formattedData.push();
+  }); */
+};
+
+const createCSV = async () => {
+  await query("cactus__items");
+  await query("cactus__options");
+
+  const items = await csv().fromFile("csv/cactus__items.csv");
+  const options = await csv().fromFile("csv/cactus__options.csv");
+  formatData(items, options);
+} 
 
 server.get("/test", (req, res) => {
   res.send('it works!');
 });
 
 server.listen(port, () => {
-  console.log("werks");
+  console.log("server started");
+  createCSV();
 });
