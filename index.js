ef61f5f2d15c77bca1c79d828e86494c53e027c3
noidsoup <nicholas.eymann@gmail.com>
2020-02-07 20:13:19 -0800

    producing final product csv for consumption by wordpress

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const fastcsv = require("fast-csv");
 const fs = require("fs");
 
 const server = express();
+server.use(express.static('csv'))
 const port = process.env.PORT || 3000;
 
-// DOE https://bezkoder.com/node-js-export-mysql-csv-file/ !!!
 // serve static file https://expressjs.com/en/starter/static-files.html
 
 var con = mysql.createConnection({
@@ -19,6 +19,7 @@ var con = mysql.createConnection({
   password: "admin"
 });
 
+// forumlates a request sent to the database
 const query = (table) => {
   return new Promise((resolve, reject) => {
     con.query(`SELECT * FROM ${table}`, function (err, data, fields) {
@@ -34,37 +35,61 @@ const query = (table) => {
         })
         .pipe(fs.createWriteStream(`csv/${table}.csv`));
   
-        
     });
   }) 
+};
+
+const returnOption = (options, id) => {
+  //console.log(options);
+  const result = options.find(option => option.ItemID === id );
+  //console.log(result);
+
+  if (result) {
+    return {
+      optionAttribuets: result.Attributes,
+    }
+  } 
 };
 
 const formatData = (items, options) => {
   console.log("here's where the formatting happens");
 
-/*   const formattedData = [];
+  const formattedData = [];
   
   items.forEach(element => {
-    console.log(element.ID);
+    //console.log(element.ID);
+    const option = returnOption(options, element.ID);
+    //console.log(option);
+    formattedData.push({ ...element, ...option });
+  });
 
-
-    formattedData.push();
-  }); */
+  return formattedData;
 };
 
+// creates the csv for the two relevant tables in the remote database
 const createCSV = async () => {
   await query("cactus__items");
   await query("cactus__options");
 
   const items = await csv().fromFile("csv/cactus__items.csv");
   const options = await csv().fromFile("csv/cactus__options.csv");
-  formatData(items, options);
+  
+  const productList = formatData(items, options);
+
+  fastcsv
+    .write(productList, { headers: true })
+    .on("finish", function() {
+      console.log(`Wrote to csv`);
+      //resolve();
+    })
+    .pipe(fs.createWriteStream(`csv/productList.csv`));
 } 
 
 server.get("/test", (req, res) => {
   res.send('it works!');
 });
 
+// Starts the server
 server.listen(port, () => {
   console.log("server started");
   createCSV();
