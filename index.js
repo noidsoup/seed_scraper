8a9acd53d0ef8347c18c6bcf5322da00ef3719b4
nicholas eymann <nicholas.eymann@gmail.com>
2020-10-17 15:29:01 -0700

    adding password

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const con = mysql.createConnection({
   host: process.env.HOST,
   database: process.env.DATABASE,
   user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
 });
 
 con.on('error', function() {});
